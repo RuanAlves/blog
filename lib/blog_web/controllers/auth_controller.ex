1e0f9aef42cd0c30a52763921a9a37422bb82c95
RuanAlves <ferreira.bessa@hotmail.com>
2021-07-25 23:55:00 -0300

    Inserindo Usuario na Secao

--- lib/blog_web/controllers/auth_controller.ex
+++ lib/blog_web/controllers/auth_controller.ex
@@ -1,5 +1,6 @@
 defmodule BlogWeb.AuthController do
   use BlogWeb, :controller
+  alias Blog.Accounts
 
   # Para usar o 'requeste`, apenas chamar esse plug
   plug Ueberauth
@@ -11,10 +12,22 @@ defmodule BlogWeb.AuthController do
       first_name: auth.info.first_name,
       last_name: auth.info.last_name,
       image: auth.info.image,
-      provider: auth.provider
+      provider: provider
     }
 
-    IO.inspect(user)
+    case Accounts.create_user(user) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Bem vindo!!! #{user.email}")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, _} ->
+        conn
+        |> put_flash(:error, "Erro ao autenticar usuário")
+        |> redirect(to: Routes.page_path(conn, :index))
+    end
+
     render(conn, "index.html")
   end
 end
